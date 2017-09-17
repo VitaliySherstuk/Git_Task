50810ab66b1c108c409fe91ef7987eaaca39c53a
Vitali Sharstuk <Vitali_Sharstuk@epam.com>
2017-09-17 21:35:20 +0300

    Second commit

---
+++ src/MasterBranch.java
@@ -0,0 +1,10 @@
+/**
+ * Created by VS-PC on 17.09.17.
+ */
+public class MasterBranch {
+
+    public void displaySecondCommit()
+    {
+        System.out.println("Second commit");
+    }
+}
--- src/Runner.java
+++ src/Runner.java
@@ -6,5 +6,8 @@ public class Runner {
     public static void main(String[] args)
     {
         System.out.println("First commit");
+
+        MasterBranch masterBranch = new MasterBranch();
+        masterBranch.displaySecondCommit();
     }
 }
