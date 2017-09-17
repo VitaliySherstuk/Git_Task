4803adb5508d1595114c051f98db4f230b8623b9
Vitali Sharstuk <Vitali_Sharstuk@epam.com>
2017-09-17 21:41:45 +0300

    First commit in Develop branch

---
+++ src/DevelopBranch.java
@@ -0,0 +1,10 @@
+/**
+ * Created by VS-PC on 17.09.17.
+ */
+public class DevelopBranch {
+
+    public void displayFirstCommitDevelopBranch()
+    {
+        System.out.println("First commit in Develop branch");
+    }
+}
